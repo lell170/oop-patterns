e3bd73314d674432201adc09aabd4b118abc39f3
Artjom <artjom.lell@web.de>
2019-10-06 22:25:50 +0200

    added adapter pattern

---
+++ src/patterns/adapter/cork/Cork.java
@@ -0,0 +1,14 @@
+package patterns.adapter.cork;
+
+public class Cork {
+
+    private final double radius;
+
+    public Cork(final double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}

176ec3d5f76568bc2fc08e7d6ad471b2ca63e9d5
Artjom Lell <artjom.lell@web.de>
2019-11-06 16:18:45 +0100

    using lambda for strategy

--- src/main/java/patterns/strategy/Main.java
+++ src/main/java/patterns/strategy/Main.java
@@ -1,7 +1,5 @@
 package patterns.strategy;
 
-import patterns.strategy.algorithms.BubbleSort;
-import patterns.strategy.algorithms.SelectionSort;
 import patterns.strategy.context.Context;
 
 import java.util.Arrays;
@@ -10,25 +8,52 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(final String[] args) {
+	public static void main(final String[] args) {
+		final List<Integer> list = Arrays.asList(4, 232, 45, 434, 67, 23, -34, 35);
+		final Context context = new Context();
 
-        final List<Integer> list = Arrays.asList(4, 232, 45, 434, 67, 23, -34, 35);
-        final Context context = new Context();
+		// using of selection sorting strategy
+		System.out.println(shuffleAndPrint(list));
+		context.setSortingAlgorithm(Main::selectionSort);
+		System.out.println(context.executeStrategy(list));
 
-        // using of selection sorting strategy
-        System.out.println(shuffleAndPrint(list));
-        context.setSortingAlgorithm(new SelectionSort());
-        System.out.println(context.executeStrategy(list));
+		// using of bubble sorting strategy
+		System.out.println(shuffleAndPrint(list));
+		context.setSortingAlgorithm(Main::bubbleSort);
+		System.out.println(context.executeStrategy(list));
+	}
 
-        // using of bubble sorting strategy
-        System.out.println(shuffleAndPrint(list));
-        context.setSortingAlgorithm(new BubbleSort());
-        System.out.println(context.executeStrategy(list));
+	private static List<Integer> shuffleAndPrint(final List<Integer> list) {
+		System.out.println("randomized List");
+		Collections.shuffle(list);
+		return list;
+	}
+
+    private static List<Integer> selectionSort(final List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            int least = i;
+            for (int j = i; j < list.size(); j++) {
+                if (list.get(j) < list.get(least)) {
+                    least = j;
+                }
+            }
+            final Integer tmp = list.get(i);
+            list.set(i, list.get(least));
+            list.set(least, tmp);
+        }
+        return list;
     }
 
-    private static List<Integer> shuffleAndPrint(final List<Integer> list) {
-        System.out.println("randomized List");
-        Collections.shuffle(list);
+    private static List<Integer> bubbleSort(final List<Integer> list) {
+        for (int i = list.size() - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    final Integer temp = list.get(j + 1);
+                    list.set(j + 1, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
         return list;
     }
 }
--- src/main/java/patterns/strategy/algorithms/SortingAlgorithm.java
+++ src/main/java/patterns/strategy/algorithms/SortingAlgorithm.java
@@ -2,6 +2,7 @@ package patterns.strategy.algorithms;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface SortingAlgorithm {
 
     List<Integer> sort(List<Integer> list);
